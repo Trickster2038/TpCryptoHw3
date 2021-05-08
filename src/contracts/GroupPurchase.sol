202c05b55dc7f745f66c6fe6a20699e797599b8b
Sergey <33185046+Trickster2038@users.noreply.github.com>
2021-05-08 21:50:29 +0300

    comments

--- src/contracts/GroupPurchase.sol
+++ src/contracts/GroupPurchase.sol
@@ -2,17 +2,20 @@ pragma solidity ^0.8.0;
 
 contract GroupPurchase {
 
-    string private _name;
-    address payable private _owner;
+    string private _name;   // name is "just-for-fun" field
+    address payable private _owner; // owner can recieve extra money
 
     struct Purchase {
         uint256 cost;
-        uint256 invested;
+        uint256 invested;   // amount of earned money
         address payable saler;
-        bool isActive;
+        bool isActive;  // shows, if Purchase was init and is not paid yet
     }
 
-    // id of purchase
+    /* 
+    contract works with multiple purchases
+    every purchase has id
+    */
     mapping (uint8 => Purchase) public purchases;
 
     constructor (string memory name_) {
@@ -28,11 +31,13 @@ contract GroupPurchase {
         return _owner;
     }
 
+    // only owner can transfer his rights
     function changeOwner(address payable owner_) public {
         require(payable(msg.sender) == _owner);
         _owner = owner_;
     }
 
+    // get the least free ID for Purchase
     function getFreeId() public view returns(uint8 i){
         i = 0;
         for (i = 1; i != 0; i++) {
@@ -44,24 +49,25 @@ contract GroupPurchase {
         return i;
     }
 
+    // inits new Purchase to start earn money for it
     function initPurchase(uint8 id_, uint256 cost_, address payable sailer_) public {
         require(purchases[id_].isActive == false);
         purchases[id_] = Purchase(cost_, 0, sailer_, true);
     }
 
-    // allows to transfer extra money to saler
+    // donate money to purchase (allows to transfer extra money to SALER)
     function investTipSaler(uint8 id_) public payable {
-        require(purchases[id_].isActive == true);
+        require(purchases[id_].isActive == true);    // checking if Purchase was init
         purchases[id_].invested += msg.value;       
-        if(purchases[id_].invested >= purchases[id_].cost){
-            purchases[id_].saler.transfer(purchases[id_].invested);
-            delete purchases[id_];
-        }
+        if(purchases[id_].invested >= purchases[id_].cost){         // if enougth money earned,
+            purchases[id_].saler.transfer(purchases[id_].invested); // saler gets his money
+            delete purchases[id_];                                  // and Purchase expires from the list
+        }                                                           // to free storage
     }
 
-    // allows to transfer extra money to owner
+    // donate money to purchase (allows to transfer extra money to OWNER)
     function investTipOwner(uint8 id_) public payable {
-        require(purchases[id_].isActive == true);
+        require(purchases[id_].isActive == true);  
         purchases[id_].invested += msg.value;       
         if(purchases[id_].invested >= purchases[id_].cost){
             uint256 tipping;
@@ -72,7 +78,7 @@ contract GroupPurchase {
         }
     }
 
-    // allows to transfer extra money back
+    // donate money to purchase (allows to transfer extra money back to SENDER)
     function investTipBack(uint8 id_) public payable {
         require(purchases[id_].isActive == true);
         purchases[id_].invested += msg.value;       
--- src/tests/test_GroupPurchase.py
+++ src/tests/test_GroupPurchase.py
@@ -3,48 +3,51 @@ from brownie import GroupPurchase
 from brownie import reverts
 import pytest
 
-def test_account_balance():
-    balance = accounts[0].balance()
-    accounts[0].transfer(accounts[1], "10 ether", gas_price=0)
-
-    assert balance - "10 ether" == accounts[0].balance()
-
+# fixture deploys Contract
 @pytest.fixture(scope="function", autouse=True)
 def init_fixture(fn_isolation):
     GroupPurchase.deploy("purchaseContract",{'from':accounts[0]})
     GroupPurchase[0].initPurchase(3, 400 , accounts[2] , {'from':accounts[1]})
 
+# test of recieving money by saler
+# if enougth money was earned
 def test_saler_got_money():
     balance = accounts[2].balance()
     t = GroupPurchase[0]
     t.investTipBack(3, {'from':accounts[3],'value':415})
     assert balance + 400 == accounts[2].balance()  
 
+# test of recieving extra money back by sender
 def test_extra_money_back():
     balance = accounts[3].balance()
     t = GroupPurchase[0]
     t.investTipBack(3, {'from':accounts[3],'value':415})
     assert balance == accounts[3].balance() + 400 
 
+# existing Purchase can't be cleared
 def test_no_reinit():
     with reverts():
         GroupPurchase[0].initPurchase(3, 400 , accounts[2] , {'from':accounts[1]})
 
+# user can't send money to non-existent Purchase
 def test_only_exist_investment():
     t = GroupPurchase[0]
     with reverts():
         t.investTipBack(5, {'from':accounts[3],'value':415})
 
+# owner can be changed
 def test_change_owner():
     t = GroupPurchase[0]
     t.changeOwner(accounts[1], {'from':accounts[0]})
     assert t.owner() == accounts[1]
 
+# ONLY owner can transfer his rights
 def test_protect_change_owner():
     t = GroupPurchase[0]
     with reverts():
         t.changeOwner(accounts[1], {'from':accounts[1]})
 
+# Purchase expires, if enougth money earned
 def test_expire_purchase():
     t = GroupPurchase[0]
     t.investTipBack(3, {'from':accounts[3],'value':415})
@@ -52,26 +55,37 @@ def test_expire_purchase():
 
 # ==================== more optional tests ===========================
 
+# ID can be reused, when Purchase expired
 def test_reinit_after_expire():
     t = GroupPurchase[0]
-    t.investTipBack(3, {'from':accounts[3],'value':415})
+    t.investTipBack(3, {'from':accounts[3],'value':415}) # cost = 400, so Purchase expires 
     GroupPurchase[0].initPurchase(3, 400 , accounts[2] , {'from':accounts[1]})
     assert True
 
+# test of recieving extra money by owner
 def test_extra_money_to_owner():
     balance = accounts[0].balance()
     t = GroupPurchase[0]
     t.investTipOwner(3, {'from':accounts[3],'value':415})
     assert balance + 415 - 400 == accounts[0].balance()
 
+# test of recieving extra money by saler
 def test_extra_money_to_saler():
     balance = accounts[2].balance()
     t = GroupPurchase[0]
     t.investTipSaler(3, {'from':accounts[3],'value':415})
     assert balance + 415 == accounts[2].balance()
 
+# test of getting free ID function
 def test_free_ids():
     t = GroupPurchase[0]
     t.initPurchase(1, 400 , accounts[2] , {'from':accounts[1]})
     t.initPurchase(2, 400 , accounts[2] , {'from':accounts[1]})
     assert t.getFreeId() == 4
+
+# saler don't get money, until all amount was earned
+def test_saler_didnt_get_money():
+    balance = accounts[2].balance()
+    t = GroupPurchase[0]
+    t.investTipBack(3, {'from':accounts[3],'value':300})
+    assert balance + 400 == accounts[2].balance()
